66fc704dd49f1166a557ea0cbee164068ba61ee0
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-03-08 20:58:04 +0530

    Update test (#2)
    * Update README
    
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>
    
    * Update .env
    
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>
    
    * Format indent
    
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>
    
    ---------
    
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+Install node modules
+
+```bash
+npm install
+```
+
+Install Playwright
+
+```bash
+npx playwright install
+```
+
+Update [.env](.env) file if needed
+
+Run tests against Selenium Grid
+
+```bash
+npx playwright test
+```
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,21 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type {PlaywrightTestConfig} from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: PlaywrightTestConfig = {
-  timeout: 1500000,
-  testMatch: ["tests/recorder.login.test.ts"],
-  use: {
-    headless: false,
-    screenshot: "on",
-    video: "on"
-  },
-  reporter: [["dot"],["json",{
-    outputFile: "jsonReports/jsonREport.json"
-  }],["html",{
-    open: "never"
-  }]]
+    timeout: 1500000,
+    testMatch: ["tests/recorder.login.test.ts"],
+    use: {
+        headless: false,
+        screenshot: "on",
+        video: "on"
+    },
+    reporter: [["dot"], ["json", {
+        outputFile: "jsonReports/jsonREport.json"
+    }], ["html", {
+        open: "never"
+    }]]
 };
 
 export default config;
